4cc1de7fc55ab09342e0cfd5f2b8ffd67fccf684
Artem <artemesin2016@gmail.com>
2020-09-08 02:58:56 +0300

    added separate class for the task 3;

---
+++ Project1/Task3.cs
@@ -0,0 +1,40 @@
+﻿namespace Project1
+{
+    class Book
+    {
+        class Title
+        {
+            private string title;
+
+            void Show()
+            {
+                
+            }
+        }
+
+        class Author
+        {
+            private string author;
+
+            void Show()
+            {
+                
+            }
+        }
+
+        class Content
+        {
+            private string content;
+
+            void Show()
+            {
+                
+            }
+        }
+    }
+    
+    public class Task3
+    {
+        
+    }
+}
